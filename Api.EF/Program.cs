bd240fd54324e88656ad6cc5011729de2e681505
Chris Hurst <cghurst@outlook.com>
2025-09-24 15:16:26 +0100

    Add swagger to dev environment

--- Api.EF/Program.cs
+++ Api.EF/Program.cs
@@ -4,6 +4,7 @@ using Api.EF.Books.Services;
 using Api.EF.Middleware;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Bookstore API",
+        Version = "v1",
+        Description = "An API for managing books and authors in a bookstore."
+    });
+});
 
 // Database
 builder.Services
@@ -23,11 +36,21 @@ builder.Services
     ;
 
 
-
 var app = builder.Build();
 
 app.UseMiddleware<LoggingMiddleware>();
 app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+// Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Bookstore API v1");
+        options.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
